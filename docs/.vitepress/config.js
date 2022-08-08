c90d64aac97f7c47712d79e50f9ebad4d6c1892c
Chen-En Yang <johnny3681@gmail.com>
2022-08-08 15:34:17 +0800

    update nav and go page

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,58 @@
 export default {
+  base: '/XNnote/',
+  lang: 'zh-Hant-TW',
   title: '小羊的學習筆記',
-  description: '日常搞笑分享.',
+  description: '搞笑日常',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: 'https://raw.githubusercontent.com/vitejs/vite/main/docs/images/vite.svg',
+      },
+    ],
+  ],
+  themeConfig: {
+    logo: 'https://raw.githubusercontent.com/vitejs/vite/main/docs/images/vite.svg',
+    nav: nav(),
+    sidebar: {
+      '/go/': sidebarGo(),
+    },
+    footer: {
+      copyright: `Copyright © ${new Date().getFullYear()} Marsgoat`,
+    },
+    outlineTitle: ' ',
+    docFooter: {
+      prev: '前一篇',
+      next: '下一篇',
+    },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/Marsgoat' }],
+  },
 };
+
+function nav() {
+  return [
+    { text: '關於', link: '/about/', activeMatch: '/about/' },
+    { text: '圍棋人', link: '/go/', activeMatch: '/go/' },
+  ];
+}
+
+function sidebarGo() {
+  return [
+    {
+      text: '圍棋',
+      collapsible: true,
+      items: [{ text: '關於', link: '/go/' }],
+    },
+    {
+      text: '圍棋人生',
+      collapsible: true,
+      items: [{ text: '圍棋替代役', link: '/go/TaiwanGoAssociation' }],
+    },
+    {
+      text: '棋評',
+      collapsible: true,
+      items: [{ text: '申之一手', link: '/go/ShinJinSeo' }],
+    },
+  ];
+}
