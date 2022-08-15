cd52d2c8cf1c8a8efbeeb33ce09ac90d25bf3ff5
Marsgoat <Marsgoat@users.noreply.github.com>
2022-08-15 09:47:36 +0000

    deploy: 1e4814a68481f189d9a86645ebb7929d35e44e7b

---
+++ assets/coding_index.md.e37261b4.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as i,o as t,a}from"./app.7bf40c82.js";const m=JSON.parse('{"title":"coding","description":"","frontmatter":{},"headers":[],"relativePath":"coding/index.md"}'),o={name:"coding/index.md"},l=a("",3),n=[l];function r(d,c,h,s,_,f){return t(),i("div",null,n)}var p=e(o,[["render",r]]);export{m as __pageData,p as default};
